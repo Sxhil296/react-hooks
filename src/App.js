59c4b7844cc28b025f781a613d74874304ca14ee
Sahil <leviack629@gmail.com>
2023-07-22 00:07:55 +0530

    data fetching using useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import IntervalHookCounter from "./components/IntervalHookCounter";
+import DataFetching from "./components/DataFetching";
 
 function App() {
-  return <div className="App">
-    <IntervalHookCounter />
-  </div>;
+  return (
+    <div className="App">
+      <DataFetching />
+    </div>
+  );
 }
 
 export default App;
