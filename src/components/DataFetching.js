ad058c31292145a257413a84b130a1cc1fd34b2d
Sahil <leviack629@gmail.com>
2023-07-22 22:51:35 +0530

    data fetching using useEffect

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -1,29 +1,39 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const DataFetching = () => {
-  const [posts, setPosts] = useState([]);
+function DataFetching() {
+	const [post, setPost] = useState({})
+	const [id, setId] = useState(1)
+	const [idFromButtonClick, setIdFromButtonClick] = useState(1)
 
-  useEffect(() => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => {
-        console.log(res);
-        setPosts(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-  return (
-    <div>
-      <ol>
-        {posts.map((post) => (
+	useEffect(() => {
+		axios
+			.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+			.then(res => {
+        console.log(res)
+        setPost(res.data)
+			})
+			.catch(err => {
+				console.log(err)
+			})
+	}, [idFromButtonClick])
+
+	const handleClick = () => {
+		setIdFromButtonClick(id)
+	}
+
+	return (
+		<div>
+			<input type="text" value={id} onChange={e => setId(e.target.value)} />
+			<button type="button" onClick={handleClick}>Fetch Post</button>
+			<div>{post.title}</div>
+			{/* <ul>
+				{posts.map(post => (
           <li key={post.id}>{post.title}</li>
-        ))}
-      </ol>
-    </div>
-  );
-};
+				))}
+			</ul> */}
+		</div>
+	)
+}
 
-export default DataFetching;
+export default DataFetching
