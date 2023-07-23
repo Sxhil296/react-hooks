9a44b07d2eaf3414aeee19533700db81550e36d3
Sahil <leviack629@gmail.com>
2023-07-23 13:16:45 +0530

    useReducer Hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import DataFetching from "./components/DataFetching";
+import CounterONe from "./components/CounterONe";
 
 function App() {
   return (
     <div className="App">
-      <DataFetching />
+      <CounterONe />
     </div>
   );
 }
